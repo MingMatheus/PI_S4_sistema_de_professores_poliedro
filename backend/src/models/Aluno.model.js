bea9b001cc4757f3530c42f6edf122cb118d9730
Matheus Ming <minggiongo11@gmail.com>
2025-10-16 00:06:17 -0300

    Criação do model de Aluno

---
+++ backend/src/models/Aluno.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const alunoSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "O email é obrigatório"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [
+      /^\S+@\S+\.\S+$/,
+      "Email inválido"
+    ]
+  },
+  senha: {
+    type: String,
+    required: [true, "A senha é obrigatória"],
+    minlength: 8,
+    select: false
+  },
+  nome: {
+    type: String,
+    required: [true, "O nome do aluno é obrigatório"],
+    trim: true
+  },
+  turma: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Turma"
+  },
+  ra: {
+    type: String,
+    required: [true, "O RA é obrigatório"],
+    trim: true
+  }
+})
