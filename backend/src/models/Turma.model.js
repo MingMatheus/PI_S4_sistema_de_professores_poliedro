09b37c18a19880c5c1ba7cb75891c09af713ae65
Matheus Ming <minggiongo11@gmail.com>
2025-10-15 23:07:22 -0300

    Criação da pasta srs e da subpasta models, bem como a criação do model de Serie

---
+++ backend/src/models/Turma.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const turmaSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    required: [true, "O nome da turma é obrigatório"],
+    unique: true,
+    trim: true
+  },
+  serie: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ""
+  }
+})
