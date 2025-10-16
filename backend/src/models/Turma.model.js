ba455e9ffbc00a3cec220124f34d11b4ea3968d8
Matheus Ming <minggiongo11@gmail.com>
2025-10-15 23:31:43 -0300

    Criação do model de Turma

---
+++ backend/src/models/Turma.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const turmaSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    required: [true, "O nome da turma é obrigatório"],
+    unique: true,
+    trim: true
+  },
+  serie: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Serie"
+  }
+})
+
+const Turma = mongoose.model("Turma", turmaSchema)
+
+module.exports = Turma
