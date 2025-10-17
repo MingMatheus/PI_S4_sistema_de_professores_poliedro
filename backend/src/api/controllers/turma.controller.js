ae760a1145efdff0c9e434a4bdfa5858afae9c19
Matheus Ming <minggiongo11@gmail.com>
2025-10-17 17:04:16 -0300

    Criação do controller de Turma

---
+++ backend/src/api/controllers/turma.controller.js
@@ -0,0 +1,33 @@
+const Turma = require("../../models/Turma.model")
+
+const codigoDeErroDeDuplicidade = 11000
+
+exports.cadastraTurma = async (req, res) => {
+  try
+  {
+    const nome = req.body.nome
+    const serie = req.body.serie
+
+    const turma = new Turma({
+      nome: nome,
+      serie: serie
+    })
+
+    await turma.save()
+  }
+  catch(error)
+  {
+    if(error.code == codigoDeErroDeDuplicidade)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
+      return res.status(409).json({mensagem: "Já existe uma turma cadastrada com esse nome"});
+
+    if(error.name == "ValidationError")  // código 400 significa bad request
+    {
+      return res.status(400).json({
+        mensagem: "Dados inválidos. Por favor, verifique os campos obrigatórios e formatos",
+        erros: error.errors
+      })
+    }
+
+    return res.status(500).json({mensagem: "Ocorreu um erro no servidor, tente novamente mais tarde"}) // código 500, internal server error
+  }
+}
