003e17e7eb387136eec86dddb0d94a65f79fc1d9
Matheus Ming <minggiongo11@gmail.com>
2025-10-17 17:26:05 -0300

    Criação do controller de Serie

---
+++ backend/src/api/controllers/serie.controller.js
@@ -0,0 +1,31 @@
+const Serie = require("../../models/Serie.model")
+
+const codigoDeErroDeDuplicidade = 11000
+
+exports.cadastraSerie = async (req, res) => {
+  try
+  {
+    const nome = req.body.nome
+
+    const serie = new Serie({
+      nome: nome
+    })
+
+    await serie.save()
+  }
+  catch(error)
+  {
+    if(error.code == codigoDeErroDeDuplicidade)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
+      return res.status(409).json({mensagem: "Já existe uma serie cadastrada com esse nome"});
+
+    if(error.name == "ValidationError")  // código 400 significa bad request
+    {
+      return res.status(400).json({
+        mensagem: "Dados inválidos. Por favor, verifique os campos obrigatórios e formatos",
+        erros: error.errors
+      })
+    }
+
+    return res.status(500).json({mensagem: "Ocorreu um erro no servidor, tente novamente mais tarde"}) // código 500, internal server error
+  }
+}
