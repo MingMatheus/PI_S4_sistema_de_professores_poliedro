c104679d3a081c297da1c2fc3fd0b832f8ca659f
Matheus Ming <minggiongo11@gmail.com>
2025-10-19 23:49:40 -0300

    Adição de 3 bibliotecas de testes e criação do jest.config.js

---
+++ backend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node', // Informa ao Jest que estamos testando um ambiente Node.js
+  coveragePathIgnorePatterns: [ // Pastas que não precisam de cobertura de teste
+    "/node_modules/"
+  ],
+}
