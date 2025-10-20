9562032c1e9f93b37594664a4b26d8abf317186f
Matheus Ming <minggiongo11@gmail.com>
2025-10-20 13:57:21 -0300

    Refatoração do controller de auth com as novas funções e constantes

--- backend/src/api/controllers/auth.controller.js
+++ backend/src/api/controllers/auth.controller.js
@@ -1,64 +1,54 @@
-const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 const Aluno = require("../../models/Aluno.model")
 const Professor = require("../../models/Professor.model")
 
-const tamanhoMinimoDaSenha = 8
-const codigoDeErroDeDuplicidade = 11000
 const regexEmailValido = /^\S+@\S+\.\S+$/
 const regexEmailDeProfessor = /.*@sistemapoliedro.com.br$/
+
+const {
+  validaEmail,
+  validaEmailProfessor
+} = require("../../utils/validators.utils")
+
+const {
+  ROLES
+} = require("../../constants/validation.constants")
+
+const {
+  MONGO_DUPLICATE_KEY,
+  MONGOOSE_VALIDATION_ERROR
+} = require("../../constants/error.constants")
 
 exports.cadastraAluno = async (req, res) => {
   try
   {
-    const email = req.body.email
+    const novoAluno = new Aluno(req.body)
 
-    if(regexEmailDeProfessor.test(email)) return res.status(400).json({mensagem: "Não é possível cadastrar um aluno com um email de professor"})  // Status 400: Bad Request
+    await novoAluno.save()
 
-    const senha = req.body.senha
-
-    if(senha.length < tamanhoMinimoDaSenha) return res.status(400).json({mensagem: `A senha deve ter no minímo ${tamanhoMinimoDaSenha} dígitos`})  // Status 400: Bad Request
-
-    const nome = req.body.nome
-    const turma = req.body.turma
-    const ra = req.body.ra
-
-    const senhaHasheada = await bcrypt.hash(senha, 10)
-
-    const aluno = new Aluno({
-      email: email,
-      senha: senhaHasheada,
-      nome: nome,
-      turma: turma,
-      ra: ra
-    })
-
-    await aluno.save()
-
-    res.status(201).end()
+    res.status(201).json({mensagem: "Aluno criado com sucesso"})
   }
   catch(error)
   {
-    if(error.code == codigoDeErroDeDuplicidade)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
+    if(error.code == MONGO_DUPLICATE_KEY)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
     {
       if(error.keyValue.email)
-      {
         return res.status(409).json({mensagem: "Esse endereço de email já está cadastrado"})
-      }
+
       if(error.keyValue.ra)
-      {
         return res.status(409).json({mensagem: "Esse RA já está cadastrado"})
-      }
 
       return res.status(409).json({mensagem: "Um campo único já existe"});
     }
 
-    if(error.name == "ValidationError")  // código 400 significa bad request
+    if(error.name == MONGOOSE_VALIDATION_ERROR)  // código 400 significa bad request
     {
+      const errorMessages = Object.values(error.errors).map(err => err.message);
+
       return res.status(400).json({
         mensagem: "Dados inválidos. Por favor, verifique os campos obrigatórios e formatos",
-        erros: error.errors
+        erros: errorMessages
       })
     }
 
@@ -69,40 +59,26 @@ exports.cadastraAluno = async (req, res) => {
 exports.cadastraProfessor = async (req, res) => {
   try
   {
-    const email = req.body.email
-
-    if(!regexEmailDeProfessor.test(email)) return res.status(400).json({mensagem: "O email fornecido não é um email de professor"})  // Status 400: Bad Request
-
-    const senha = req.body.senha
-
-    if(senha.length < tamanhoMinimoDaSenha) return res.status(400).json({mensagem: `A senha deve ter no minímo ${tamanhoMinimoDaSenha} dígitos`})  // Status 400: Bad Request
-
-    const nome = req.body.nome
-
-    const senhaHasheada = await bcrypt.hash(senha, 10)
-
-    const professor = new Professor({
-      email: email,
-      senha: senhaHasheada,
-      nome: nome
-    })
+    const professor = new Professor(req.body)
 
     await professor.save()
 
-    res.status(201).end()
+    res.status(201).json({mensagem: "Professor criado com sucesso"})
   }
   catch(error)
   {
-    if(error.code == codigoDeErroDeDuplicidade)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
+    if(error.code == MONGO_DUPLICATE_KEY)  // Retorna um código 409, que indica conflito (de unicidade nesse caso)
     {
       return res.status(409).json({mensagem: "Já existe um professor cadastrado com esse email"});
     }
 
-    if(error.name == "ValidationError")  // código 400 significa bad request
+    if(error.name == MONGOOSE_VALIDATION_ERROR)  // código 400 significa bad request
     {
+      const errorMessages = Object.values(error.errors).map(err => err.message);
+
       return res.status(400).json({
         mensagem: "Dados inválidos. Por favor, verifique os campos obrigatórios e formatos",
-        erros: error.errors
+        erros: errorMessages
       })
     }
 
@@ -115,20 +91,20 @@ exports.login = async (req, res) => {
   const senha = req.body.senha
 
   // Testa se o email enviado é um email válido
-  if(!regexEmailValido.test(email))
+  if(!validaEmail(email))
     return res.status(400).json({mensagem: "Email inválido"})  // Status 400: Bad Request
 
   let usuario, role
   // Verifica se o email é de professor ou de aluno
-  if(regexEmailDeProfessor.test(email))
+  if(validaEmailProfessor(email))
   {
     usuario = await Professor.findOne({email: email}).select("+senha")
-    role = "professor"
+    role = ROLES.PROFESSOR
   }
   else
   {
     usuario = await Aluno.findOne({email: email}).select("+senha")
-    role = "aluno"
+    role = ROLES.ALUNO
   }
 
   if(!usuario)
@@ -150,5 +126,5 @@ exports.login = async (req, res) => {
     {expiresIn: "1h"}
   )
 
-  res.status(200).json({token: token})
+  res.status(200).json({mensagem: "Login realizado com sucesso", token: token})
 }
