c551c5523ea6f55212297ffbcd0bf09a33131933
Matheus Ming <minggiongo11@gmail.com>
2025-10-20 02:03:21 -0300

    Criação da pasta utils e do arquivo validators.utils.js

---
+++ backend/src/utils/validators.utils.js
@@ -0,0 +1,29 @@
+const {
+  TAMANHO_MINIMO_SENHA,
+  REGEX_EMAIL_GERAL,
+  REGEX_EMAIL_PROFESSOR
+} = require("../../constants/validation.constants")
+
+const validaEmail = (email) => {
+  if(!email) return false
+
+  return REGEX_EMAIL_GERAL.test(email)
+}
+
+const validaEmailProfessor = (emailProfessor) => {
+  if(!emailProfessor) return false
+
+  return REGEX_EMAIL_GERAL.test(emailProfessor) && REGEX_EMAIL_PROFESSOR.test(emailProfessor)
+}
+
+const validaSenha = (senha) => {
+  if(!senha) return false
+
+  return senha.legth >= TAMANHO_MINIMO_SENHA
+}
+
+module.exports = {
+  validaEmail,
+  validaEmailProfessor,
+  validaSenha
+}
