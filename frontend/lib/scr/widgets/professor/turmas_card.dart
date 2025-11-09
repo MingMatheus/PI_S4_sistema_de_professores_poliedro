18f5d561774fb0578a20e2d953f65c1d5bc632d8
julian1818 <junatacastelli@gmail.com>
2025-11-09 15:32:43 -0300

    Criação do widgets turmas_card.dart

---
+++ frontend/lib/scr/widgets/professor/turmas_card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'professor_option_card.dart';
+
+class ProfessorTurmasCard extends StatelessWidget {
+  const ProfessorTurmasCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProfessorOptionCard(
+      icon: Icons.people_alt_rounded,
+      color: const Color(0xFF1976D2),
+      title: 'Turmas',
+      subtitle: 'Criar, editar e gerenciar turmas e alunos.',
+      onTap: () {
+        // depois: navegar para tela de turmas do professor
+      },
+    );
+  }
+}
