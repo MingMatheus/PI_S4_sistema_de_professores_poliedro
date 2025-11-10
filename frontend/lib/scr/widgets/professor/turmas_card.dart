83cd4c223ce1dd424d6b9c802b5c15a2cc85d33b
julian1818 <junatacastelli@gmail.com>
2025-11-09 22:28:40 -0300

    adiciona ProfessorTurmasCard para navegação à tela de turmas

--- frontend/lib/scr/widgets/professor/turmas_card.dart
+++ frontend/lib/scr/widgets/professor/turmas_card.dart
@@ -7,8 +7,7 @@ class ProfessorTurmasCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ProfessorOptionCard(
-      icon: Icons.people_alt_rounded,
-      color: const Color(0xFF1976D2),
+      icon: Icons.people_alt_outlined,
       title: 'Turmas',
       subtitle: 'Criar, editar e gerenciar turmas e alunos.',
       onTap: () {
