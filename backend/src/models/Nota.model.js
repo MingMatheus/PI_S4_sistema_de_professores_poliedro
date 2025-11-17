e4a43fdc77afa23c1a6827abbab3ad91aab5a6e9
Matheus Ming <minggiongo11@gmail.com>
2025-11-17 19:58:36 -0300

    Pequena correção no model de nota

--- backend/src/models/Nota.model.js
+++ backend/src/models/Nota.model.js
@@ -29,29 +29,6 @@ const notaSchema = mongoose.Schema({
 
 notaSchema.index({aluno: 1, avaliacao: 1}, {unique: true})
 
-notaSchema.pre("save", async function(next) {
-  const aluno = this
-
-  if (!aluno.isModified("senha"))
-    return next();
-
-  // 1. Valida a senha
-  if(!validaSenha(aluno.senha))
-    return next(new Error(VALIDACAO.GERAL.SENHA_INVALIDA))
-
-  // 2. Faz o hashing
-  try
-  {
-    const senhaHasheada = await bcrypt.hash(aluno.senha, 10)
-    aluno.senha = senhaHasheada
-    next()
-  }
-  catch(error)
-  {
-    next(error);
-  }
-})
-
 const Nota = mongoose.model("Nota", notaSchema)
 
 module.exports = Nota
